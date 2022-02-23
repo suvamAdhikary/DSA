3a574e864c2b70b7dfecf6e3871a9460cb609732
Suvam Adhikary <suvamadhikary@Suvams-MacBook-Pro.local>
2022-02-23 18:54:34 +0530

    Path Sum - Binary Tree - Tree - DFS - Easy

---
+++ DSA/Data_Structure/Binary_Tree/Easy/Path_Sum/Solutions/Solve.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+ var hasPathSum = function(root, targetSum) {
+    if(!root) return false;
+    return helper(root, targetSum);
+};
+
+const helper = (root, target) => {
+    if(!root) return false;
+    if(!root.left && !root.right && target - root.val === 0) return true;
+    return helper(root.left, target - root.val) || helper(root.right, target - root.val);
+}
+
