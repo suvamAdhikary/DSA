71b3eb1acea2296e4886be2afb4a33e2caddb8ae
Suvam Adhikary <suvamadhikary@Suvams-MacBook-Pro.local>
2022-03-07 18:05:57 +0530

    folder

--- DSA/Data_Structure/String/Easy/Solve.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} sentence
- * @return {boolean}
- */
-var checkIfPangram = function(sentence) {
-   let set = new Set(sentence);
-    console.log(set);
-    return set.size === 26;
-};
